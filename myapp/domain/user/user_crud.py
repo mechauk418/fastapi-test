b1b6fbc469ed1d1ed891f57e0ab8bf15c27da5b7
mechauk418 <mechauk418@gmail.com>
2023-12-28 13:33:10 +0900

    1228

--- myapp/domain/user/user_crud.py
+++ myapp/domain/user/user_crud.py
@@ -1,5 +1,5 @@
 from passlib.context import CryptContext
-
+from passlib.hash import bcrypt
 from sqlalchemy.orm import Session
 from domain.user.user_schema import UserCreate
 from models import User
@@ -13,3 +13,12 @@ def create_user(db: Session, user_create: UserCreate):
                    email=user_create.email)
     db.add(db_user)
     db.commit()
+
+def get_existing_user(db: Session, user_create: UserCreate):
+    return db.query(User).filter(
+        (User.username == user_create.username) |
+        (User.email == user_create.email)
+    ).first()
+
+def get_user(db:Session, username:str):
+    return db.query(User).filter(User.username == username).first()
